fa47784dda063f944d320e9e20e8a78e05a76181
Miguel Rodriguez <miguelrod2203@gmail.com>
2022-12-26 22:47:51 -0300

    simulador listo

--- javascript/main.js
+++ javascript/main.js
@@ -1,7 +1,6 @@
 "use strict" 
 alert('hola. Bienvenido a SW donde encontraras los mejores smartWatch.');
-
-
+console.log("Resumen de compra");
 
 // productos con su precio
 
@@ -24,7 +23,6 @@ let producto6 = "Smartwatch Instinct 2";
 let precioProducto6 = 599990;
 
 
-
 // seleccion de productos de la lista por numeracion 
 
 let productoSeleccionado = Number(prompt(`Indicame en cual de nuestros 6 productos estas interesado
@@ -36,34 +34,47 @@ let productoSeleccionado = Number(prompt(`Indicame en cual de nuestros 6 product
 6. ${producto6} $ ${precioProducto6}
 `));
 
-
-
-// mostrando el resumen de la compra dependiendo de la seleccion
+while(productoSeleccionado <= 0 || productoSeleccionado > 6){
+    alert("el valor ingresado no es valido intente un numero del 1 al 6");
+    productoSeleccionado = Number(prompt(`Indicame en cual de nuestros 6 productos estas interesado`));
+}
 
 if (productoSeleccionado > 0 ){
     switch (productoSeleccionado){
         case 1 :
             alert(`Usted ah seleccionado el producto ${productoSeleccionado}. ${producto1} por $ ${precioProducto1}`);
+            console.log(`Producto : ${producto1}`);
+            console.log(`Precio : ${precioProducto1}`);
             break;
 
         case 2 :
             alert(`Usted ah seleccionado el producto ${productoSeleccionado}. ${producto2} por $ ${precioProducto2}`);
+            console.log(`Producto : ${producto2}`);
+            console.log(`Precio : ${precioProducto2}`);
             break;
 
         case 3 :
             alert(`Usted ah seleccionado el producto ${productoSeleccionado}. ${producto3} por $ ${precioProducto3}`);
+            console.log(`Producto : ${producto3}`);
+            console.log(`Precio : ${precioProducto3}`);
             break;
 
         case 4 :
             alert(`Usted ah seleccionado el producto ${productoSeleccionado}. ${producto4} por $ ${precioProducto4}`);
+            console.log(`Producto : ${producto4}`);
+            console.log(`Precio : ${precioProducto4}`);
             break;
 
         case 5 :
             alert(`Usted ah seleccionado el producto ${productoSeleccionado}. ${producto5} por $ ${precioProducto5}`);
+            console.log(`Producto : ${producto5}`);
+            console.log(`Precio : ${precioProducto5}`);
             break;
 
         case 6 :
             alert(`Usted ah seleccionado el producto ${productoSeleccionado}. ${producto6} por $ ${precioProducto6}`);
+            console.log(`Producto : ${producto6}`);
+            console.log(`Precio : ${precioProducto6}`);
             break;
     
         default: 
@@ -71,73 +82,101 @@ if (productoSeleccionado > 0 ){
             break;           
     }
 } else{
-    alert("producto no encontrado")
+    alert("Producto no encontrado");
 }
-
 
 
 // solicitando datos del comprador (nombre)
 
 let usuario = prompt("Ingresa tu nombre");
 
-while((usuario.length < 3)){
-    alert('Debes incluir por lo menos 3 caracteres');
-    usuario = prompt("Intenta ingresando tu nombre y apelido");
-}
-alert(usuario);
+function validarNombre (){
 
+    while((usuario.length < 3 )){
+        alert('Debes incluir por lo menos 3 caracteres');
+        usuario = prompt("Intenta ingresando tu nombre y apelido");
+    }
+    alert(`Hemos registrado correctamente el nombre : ${usuario}`);
+    console.log(`Cliente : ${usuario}`);
+    return usuario;
+}
+
+validarNombre() ;
 
 
 // solicitando datos del comprador (direccion)
 
 let direccion = prompt("Ingrese su direccion");
 
-while((direccion.length < 6)){
-    alert('Debes incluir por lo menos 6 caracteres');
-    direccion = prompt("Intenta colocando el nombre de la calle y su numero");
+function validarDireccion () {
+
+    while((direccion.length < 6)){
+        alert('Debes incluir por lo menos 6 caracteres');
+        direccion = prompt("Intenta colocando el nombre de la calle y su numero");
+    }
+    alert(`Hemos registrado correctamente su direccion en ${direccion}`);
+    console.log(`Direccion : ${direccion}`);
+    return direccion;
 }
-alert(direccion);
 
-
+validarDireccion()
 
 // solicitando datos del comprador (correo y confirmacion de correo)
 
 let correo = prompt("Ingrese su correo");
-alert("hola para continuar queremos confirmar tu correo");
+alert("Para continuar queremos confirmar tu correo");
 let correoConfirmado = "";
 
-do {
-    correoConfirmado = prompt("Confirma tu correo");
-    if (correo === correoConfirmado){
-        alert("Genial se ha confirmado correctamente tu correo");
-    }else{
-        alert("correo ingresado no coincide");
-    }
-}while(correo != correoConfirmado);
+function validarCorreo (){
 
+    do {
+        correoConfirmado = prompt("Confirma tu correo");
+        if (correo === correoConfirmado){
+            alert("Genial se ha confirmado correctamente tu correo");
+            console.log(`correo : ${correo}`);
+        }else{
+            alert("correo ingresado no coincide");
+        }
+    }while(correo != correoConfirmado);
+
+    return correo;
+}
+
+validarCorreo();
 
 
 // tipo de entrega
 
-let envio = 3000;
+let envio = "gratis";
 let retiroEnTienda = "gratis";
 
 let entrega = prompt(`Elige un tipo de entrega
-1. Envio $ ${envio};
+1. Envio a domicilio $ ${envio};
 2. Retiro en tienda ${retiroEnTienda};
 `);
 
-while(entrega != 1 && entrega != 2){
-    entrega = prompt(`Ingresa una opcion valida`)
-}    
+function validarEntrega (){
 
-if (entrega == 1){
+    while(entrega != 1 && entrega != 2){
+        entrega = prompt(`Ingresa una opcion valida
+        1. Envio a domicilio ${envio};
+        2. Retiro en tienda ${retiroEnTienda};
+        `)
+    }    
+
+    if (entrega == 1){
         alert(`Envio por $ ${envio}`)
+        console.log(`Tipo de entrega : Envio a domicilio`);
     }
-else if(entrega == 2){
-    alert(`Retiro en tienda ${retiroEnTienda}`)
+    else if(entrega == 2){
+        alert(`Retiro en tienda ${retiroEnTienda}`)
+        console.log(`Tipo de entrega : Retiro en tienda`);
+    }
+
+    return entrega;
 }
 
+validarEntrega();
 
 
 // seleccionar tipo de pago
@@ -146,24 +185,40 @@ let tipoDePago = Number(prompt(`Elige el tipo de pago
 1. Efectivo.
 2. Debito.
 3. Credito.
-`))
+`));
 
-while(tipoDePago <= 0  || tipoDePago >= 4){
-    tipoDePago = prompt(`Ingresa una opcion valida`)
-}   
+function validarFormaDePago (){
 
-if (tipoDePago == 1){
-    alert(`Pago con Efectivo Ingresado`);
+    while(tipoDePago <= 0  || tipoDePago >= 4){
+        tipoDePago = prompt(`Ingresa una opcion valida
+        1. Efectivo.
+        2. Debito.
+        3. Credito.
+        `)
+    }   
+
+    if (tipoDePago == 1){
+        alert(`Pago con Efectivo Ingresado`);
+        console.log(`Tipo de pago : Efectivo`);
+    }
+    else if (tipoDePago == 2){
+        alert(`Pago con Debito Ingresado`);
+        console.log(`Tipo de pago : Debito`);
+    }
+    else if (tipoDePago == 3){
+        alert(`Pago con Credito Ingresado`);
+        console.log(`Tipo de pago : Credito`);
+    }
+
+    alert("Magicamente su pago se ha registrado correctamente");
 }
-else if (tipoDePago == 2){
-    alert(`Pago con Debito Ingresado`);
-}
-else if (tipoDePago == 3){
-    alert(`Pago con Credito Ingresado`);
-}
 
-alert("magicamente su pago se ha registrado correctamente");
+validarFormaDePago();
 
-
-
-
+/*
+************* NOTA*************
+todos los resultados de la simulacion tambien
+se pueden ir observando en consola de esta
+manera no se pierde la informacion seleccionada
+despues de avanzar a la siguiente linea.
+*/
