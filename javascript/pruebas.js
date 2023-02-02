a2951f8f562425f4ea5ceb25132e8b0e559781c3
Miguel Rodriguez <miguelrod2203@gmail.com>
2023-02-01 22:14:20 -0300

    rutas de nuevo

--- javascript/pruebas.js
+++ javascript/pruebas.js
@@ -1,71 +1,71 @@
-// aqui pruebo las funciones de manera independiente para no recorrer todo el programa
-class Producto {
-    constructor(id, marca, nombre, color, imagen, pantalla, genero, precio, sistemaOperativo){
-        this.id = id;
-        this.marca = marca;
-        this.nombre = nombre;
-        this.color = color;
-        this.imagen = imagen;
-        this.pantalla = pantalla
-        this.genero = genero;
-        this.precio = precio;
-        this.sistemaOperativo = sistemaOperativo;
-    }
-}
+// // aqui pruebo las funciones de manera independiente para no recorrer todo el programa
+// class Producto {
+//     constructor(id, marca, nombre, color, imagen, pantalla, genero, precio, sistemaOperativo){
+//         this.id = id;
+//         this.marca = marca;
+//         this.nombre = nombre;
+//         this.color = color;
+//         this.imagen = imagen;
+//         this.pantalla = pantalla
+//         this.genero = genero;
+//         this.precio = precio;
+//         this.sistemaOperativo = sistemaOperativo;
+//     }
+// }
 
-let producto1 = new Producto( '1', 'APPLE', 'Apple Watch Se (44Mm, Gps)', 'Azul media noche', '/imagenes/smarwatch01.jpg', 'Digital', 'Unisex', 339990, 'IOS');
-let producto2 = new Producto( '2', 'APPLE', 'Apple Watch Ultra (49mm, GPS + Cellular)', 'Blanco estelar', '/imagenes/smarwatch02.jpg', 'Digital', 'Unisex', 1079990, 'IOS');
-let producto3 = new Producto( '3', 'APPLE', 'Apple Watch series 7 (41mm, GPS)', 'Rojo', '/imagenes/smarwatch03.jpg', 'Digital', 'Unisex', 429990, 'IOS');
-let producto4 = new Producto( '4', 'APPLE', 'Apple Watch Series 3 (42Mm, Gps)', 'Negra', '/imagenes/smarwatch04.jpg', 'Digital', 'Unisex', 249990, 'IOS');
-let producto5 = new Producto( '5', 'APPLE', 'Apple Watch Series 8 (45mm, GPS)', 'Blanco estelar', '/imagenes/smarwatch05.jpg', 'Digital', 'Unisex', 549990, 'IOS');
+// let producto1 = new Producto( '1', 'APPLE', 'Apple Watch Se (44Mm, Gps)', 'Azul media noche', '/imagenes/smarwatch01.jpg', 'Digital', 'Unisex', 339990, 'IOS');
+// let producto2 = new Producto( '2', 'APPLE', 'Apple Watch Ultra (49mm, GPS + Cellular)', 'Blanco estelar', '/imagenes/smarwatch02.jpg', 'Digital', 'Unisex', 1079990, 'IOS');
+// let producto3 = new Producto( '3', 'APPLE', 'Apple Watch series 7 (41mm, GPS)', 'Rojo', '/imagenes/smarwatch03.jpg', 'Digital', 'Unisex', 429990, 'IOS');
+// let producto4 = new Producto( '4', 'APPLE', 'Apple Watch Series 3 (42Mm, Gps)', 'Negra', '/imagenes/smarwatch04.jpg', 'Digital', 'Unisex', 249990, 'IOS');
+// let producto5 = new Producto( '5', 'APPLE', 'Apple Watch Series 8 (45mm, GPS)', 'Blanco estelar', '/imagenes/smarwatch05.jpg', 'Digital', 'Unisex', 549990, 'IOS');
 
-let listaDeProductos = [producto1, producto2, producto3, producto4, producto5]
+// let listaDeProductos = [producto1, producto2, producto3, producto4, producto5]
 
 
 
-const nuestrosProductos = document.querySelector('#nuestrosProductos');
+// const nuestrosProductos = document.querySelector('#nuestrosProductos');
 
-let carrito = [];
+// let carrito = [];
 
-const renderizarNuestrosProductos = () => {
-    listaDeProductos.forEach((producto) => {
-        const cardProducto = document.createElement('div')
-        cardProducto.classList.add('card_producto')
-        cardProducto.setAttribute('data-id', producto.id)
-        cardProducto.innerHTML = `
-            <div class="imagen_producto">
-                <a href="./html/verMas.html">
-                    <img src="${producto.imagen}" alt="${producto.nombre}" title="${producto.nombre}">
-                </a>    
-                    <hr>
-            </div>
+// const renderizarNuestrosProductos = () => {
+//     listaDeProductos.forEach((producto) => {
+//         const cardProducto = document.createElement('div')
+//         cardProducto.classList.add('card_producto')
+//         cardProducto.setAttribute('data-id', producto.id)
+//         cardProducto.innerHTML = `
+//             <div class="imagen_producto">
+//                 <a href="./html/verMas.html">
+//                     <img src="${producto.imagen}" alt="${producto.nombre}" title="${producto.nombre}">
+//                 </a>    
+//                     <hr>
+//             </div>
             
-            <div class="Informacion_producto">
-                <p> 
-                    ${producto.marca} <br>
-                    ${producto.nombre} <br>
-                    $ ${producto.precio}.
-                </p>
-            </div>
+//             <div class="Informacion_producto">
+//                 <p> 
+//                     ${producto.marca} <br>
+//                     ${producto.nombre} <br>
+//                     $ ${producto.precio}.
+//                 </p>
+//             </div>
 
-            <div class="boton_comprar d-grid">
-                <button class="btn justify-content-md-center btn-outline-success" type="submit">Agregar al Carro</button>
-            </div>
-            `
-        nuestrosProductos.append(cardProducto)
+//             <div class="boton_comprar d-grid">
+//                 <button class="btn justify-content-md-center btn-outline-success" type="submit">Agregar al Carro</button>
+//             </div>
+//             `
+//         nuestrosProductos.append(cardProducto)
 
-    })
+//     })
     
-    const verMasDelProducto = document.querySelectorAll('.card_producto')
-    verMasDelProducto.forEach((verMas) => {
-        verMas.addEventListener('click', detallesProducto)
-    })
+//     const verMasDelProducto = document.querySelectorAll('.card_producto')
+//     verMasDelProducto.forEach((verMas) => {
+//         verMas.addEventListener('click', detallesProducto)
+//     })
 
 
-}
+// }
 
 
 
-renderizarNuestrosProductos();
+// renderizarNuestrosProductos();
 
 
--- javascript/simulador1.js
+++ javascript/simulador1.js
@@ -1,229 +1,229 @@
-"use strict" 
+// "use strict" 
 
-// este simulador contempla los temas de
-// variables / control de flujo / ciclos / funciones
+// // este simulador contempla los temas de
+// // variables / control de flujo / ciclos / funciones
 
-alert('hola. Bienvenido a SW donde encontraras los mejores smartWatch.');
-console.log("Resumen de compra");
+// alert('hola. Bienvenido a SW donde encontraras los mejores smartWatch.');
+// console.log("Resumen de compra");
 
-// productos con su precio
+// // productos con su precio
 
-let producto1 = "Appe Watch Ultra";
-let precioProducto1 = 999990;
+// let producto1 = "Appe Watch Ultra";
+// let precioProducto1 = 999990;
 
-let producto2 = "Smartwatch Fenix 6X";
-let precioProducto2 = 849990;
+// let producto2 = "Smartwatch Fenix 6X";
+// let precioProducto2 = 849990;
 
-let producto3 = "Smartwatch Suunto 9";
-let precioProducto3 = 637490;
+// let producto3 = "Smartwatch Suunto 9";
+// let precioProducto3 = 637490;
 
-let producto4 = "Smartwatch Forerunner 955";
-let precioProducto4 = 749990;
+// let producto4 = "Smartwatch Forerunner 955";
+// let precioProducto4 = 749990;
 
-let producto5 = "Appe Watch Series 8";
-let precioProducto5 = 719990;
+// let producto5 = "Appe Watch Series 8";
+// let precioProducto5 = 719990;
 
-let producto6 = "Smartwatch Instinct 2";
-let precioProducto6 = 599990;
+// let producto6 = "Smartwatch Instinct 2";
+// let precioProducto6 = 599990;
 
 
-// seleccion de productos de la lista por numeracion 
+// // seleccion de productos de la lista por numeracion 
 
-let productoSeleccionado = Number(prompt(`Indicame en cual de nuestros 6 productos estas interesado
-1. ${producto1} $ ${precioProducto1}
-2. ${producto2} $ ${precioProducto2}
-3. ${producto3} $ ${precioProducto3}
-4. ${producto4} $ ${precioProducto4}
-5. ${producto5} $ ${precioProducto5} 
-6. ${producto6} $ ${precioProducto6}
-`));
+// let productoSeleccionado = Number(prompt(`Indicame en cual de nuestros 6 productos estas interesado
+// 1. ${producto1} $ ${precioProducto1}
+// 2. ${producto2} $ ${precioProducto2}
+// 3. ${producto3} $ ${precioProducto3}
+// 4. ${producto4} $ ${precioProducto4}
+// 5. ${producto5} $ ${precioProducto5} 
+// 6. ${producto6} $ ${precioProducto6}
+// `));
 
-while(productoSeleccionado <= 0 || productoSeleccionado > 6){
-    alert("el valor ingresado no es valido intente un numero del 1 al 6");
-    productoSeleccionado = Number(prompt(`Indicame en cual de nuestros 6 productos estas interesado`));
-}
+// while(productoSeleccionado <= 0 || productoSeleccionado > 6){
+//     alert("el valor ingresado no es valido intente un numero del 1 al 6");
+//     productoSeleccionado = Number(prompt(`Indicame en cual de nuestros 6 productos estas interesado`));
+// }
 
-if (productoSeleccionado > 0 ){
-    switch (productoSeleccionado){
-        case 1 :
-            alert(`Usted ah seleccionado el producto ${productoSeleccionado}. ${producto1} por $ ${precioProducto1}`);
-            console.log(`Producto : ${producto1}`);
-            console.log(`Precio : ${precioProducto1}`);
-            break;
+// if (productoSeleccionado > 0 ){
+//     switch (productoSeleccionado){
+//         case 1 :
+//             alert(`Usted ah seleccionado el producto ${productoSeleccionado}. ${producto1} por $ ${precioProducto1}`);
+//             console.log(`Producto : ${producto1}`);
+//             console.log(`Precio : ${precioProducto1}`);
+//             break;
 
-        case 2 :
-            alert(`Usted ah seleccionado el producto ${productoSeleccionado}. ${producto2} por $ ${precioProducto2}`);
-            console.log(`Producto : ${producto2}`);
-            console.log(`Precio : ${precioProducto2}`);
-            break;
+//         case 2 :
+//             alert(`Usted ah seleccionado el producto ${productoSeleccionado}. ${producto2} por $ ${precioProducto2}`);
+//             console.log(`Producto : ${producto2}`);
+//             console.log(`Precio : ${precioProducto2}`);
+//             break;
 
-        case 3 :
-            alert(`Usted ah seleccionado el producto ${productoSeleccionado}. ${producto3} por $ ${precioProducto3}`);
-            console.log(`Producto : ${producto3}`);
-            console.log(`Precio : ${precioProducto3}`);
-            break;
+//         case 3 :
+//             alert(`Usted ah seleccionado el producto ${productoSeleccionado}. ${producto3} por $ ${precioProducto3}`);
+//             console.log(`Producto : ${producto3}`);
+//             console.log(`Precio : ${precioProducto3}`);
+//             break;
 
-        case 4 :
-            alert(`Usted ah seleccionado el producto ${productoSeleccionado}. ${producto4} por $ ${precioProducto4}`);
-            console.log(`Producto : ${producto4}`);
-            console.log(`Precio : ${precioProducto4}`);
-            break;
+//         case 4 :
+//             alert(`Usted ah seleccionado el producto ${productoSeleccionado}. ${producto4} por $ ${precioProducto4}`);
+//             console.log(`Producto : ${producto4}`);
+//             console.log(`Precio : ${precioProducto4}`);
+//             break;
 
-        case 5 :
-            alert(`Usted ah seleccionado el producto ${productoSeleccionado}. ${producto5} por $ ${precioProducto5}`);
-            console.log(`Producto : ${producto5}`);
-            console.log(`Precio : ${precioProducto5}`);
-            break;
+//         case 5 :
+//             alert(`Usted ah seleccionado el producto ${productoSeleccionado}. ${producto5} por $ ${precioProducto5}`);
+//             console.log(`Producto : ${producto5}`);
+//             console.log(`Precio : ${precioProducto5}`);
+//             break;
 
-        case 6 :
-            alert(`Usted ah seleccionado el producto ${productoSeleccionado}. ${producto6} por $ ${precioProducto6}`);
-            console.log(`Producto : ${producto6}`);
-            console.log(`Precio : ${precioProducto6}`);
-            break;
+//         case 6 :
+//             alert(`Usted ah seleccionado el producto ${productoSeleccionado}. ${producto6} por $ ${precioProducto6}`);
+//             console.log(`Producto : ${producto6}`);
+//             console.log(`Precio : ${precioProducto6}`);
+//             break;
     
-        default: 
-            alert(`El producto ${productoSeleccionado}, no existe o no esta habilitada`);
-            break;           
-    }
-} else{
-    alert("Producto no encontrado");
-}
+//         default: 
+//             alert(`El producto ${productoSeleccionado}, no existe o no esta habilitada`);
+//             break;           
+//     }
+// } else{
+//     alert("Producto no encontrado");
+// }
 
 
-// solicitando datos del comprador (nombre)
+// // solicitando datos del comprador (nombre)
 
-let usuario = prompt("Ingresa tu nombre");
+// let usuario = prompt("Ingresa tu nombre");
 
-function validarNombre (){
+// function validarNombre (){
 
-    while((usuario.length < 3 )){
-        alert('Debes incluir por lo menos 3 caracteres');
-        usuario = prompt("Intenta ingresando tu nombre y apelido");
-    }
-    alert(`Hemos registrado correctamente el nombre : ${usuario}`);
-    console.log(`Cliente : ${usuario}`);
-    return usuario;
-}
+//     while((usuario.length < 3 )){
+//         alert('Debes incluir por lo menos 3 caracteres');
+//         usuario = prompt("Intenta ingresando tu nombre y apelido");
+//     }
+//     alert(`Hemos registrado correctamente el nombre : ${usuario}`);
+//     console.log(`Cliente : ${usuario}`);
+//     return usuario;
+// }
 
-validarNombre() ;
-
-
-// solicitando datos del comprador (direccion)
-
-let direccion = prompt("Ingrese su direccion");
-
-function validarDireccion () {
-
-    while((direccion.length < 6)){
-        alert('Debes incluir por lo menos 6 caracteres');
-        direccion = prompt("Intenta colocando el nombre de la calle y su numero");
-    }
-    alert(`Hemos registrado correctamente su direccion en ${direccion}`);
-    console.log(`Direccion : ${direccion}`);
-    return direccion;
-}
-
-validarDireccion()
-
-// solicitando datos del comprador (correo y confirmacion de correo)
-
-let correo = prompt("Ingrese su correo");
-alert("Para continuar queremos confirmar tu correo");
-let correoConfirmado = "";
-
-function validarCorreo (){
-
-    do {
-        correoConfirmado = prompt("Confirma tu correo");
-        if (correo === correoConfirmado){
-            alert("Genial se ha confirmado correctamente tu correo");
-            console.log(`correo : ${correo}`);
-        }else{
-            alert("correo ingresado no coincide");
-        }
-    }while(correo != correoConfirmado);
-
-    return correo;
-}
-
-validarCorreo();
+// validarNombre() ;
 
 
-// tipo de entrega
+// // solicitando datos del comprador (direccion)
 
-let envio = "gratis";
-let retiroEnTienda = "gratis";
+// let direccion = prompt("Ingrese su direccion");
 
-let entrega = prompt(`Elige un tipo de entrega
-1. Envio a domicilio : ${envio}
-2. Retiro en tienda : ${retiroEnTienda}
-`);
+// function validarDireccion () {
 
-function validarEntrega (){
+//     while((direccion.length < 6)){
+//         alert('Debes incluir por lo menos 6 caracteres');
+//         direccion = prompt("Intenta colocando el nombre de la calle y su numero");
+//     }
+//     alert(`Hemos registrado correctamente su direccion en ${direccion}`);
+//     console.log(`Direccion : ${direccion}`);
+//     return direccion;
+// }
 
-    while(entrega != 1 && entrega != 2){
-        entrega = prompt(`Ingresa una opcion valida
-        1. Envio a domicilio : ${envio}
-        2. Retiro en tienda : ${retiroEnTienda}
-        `);
-    }    
+// validarDireccion()
 
-    if (entrega == 1){
-        alert(`Envio a domicilio ${envio}`);
-        console.log(`Tipo de entrega : Envio a domicilio`);
-    }
-    else if(entrega == 2){
-        alert(`Retiro en tienda ${retiroEnTienda}`);
-        console.log(`Tipo de entrega : Retiro en tienda`);
-    }
+// // solicitando datos del comprador (correo y confirmacion de correo)
 
-    return entrega;
-}
+// let correo = prompt("Ingrese su correo");
+// alert("Para continuar queremos confirmar tu correo");
+// let correoConfirmado = "";
 
-validarEntrega();
+// function validarCorreo (){
+
+//     do {
+//         correoConfirmado = prompt("Confirma tu correo");
+//         if (correo === correoConfirmado){
+//             alert("Genial se ha confirmado correctamente tu correo");
+//             console.log(`correo : ${correo}`);
+//         }else{
+//             alert("correo ingresado no coincide");
+//         }
+//     }while(correo != correoConfirmado);
+
+//     return correo;
+// }
+
+// validarCorreo();
 
 
-// seleccionar tipo de pago
+// // tipo de entrega
 
-let tipoDePago = Number(prompt(`Elige el tipo de pago
-1. Efectivo.
-2. Debito.
-3. Credito.
-`));
+// let envio = "gratis";
+// let retiroEnTienda = "gratis";
 
-function validarFormaDePago (){
+// let entrega = prompt(`Elige un tipo de entrega
+// 1. Envio a domicilio : ${envio}
+// 2. Retiro en tienda : ${retiroEnTienda}
+// `);
 
-    while(tipoDePago <= 0  || tipoDePago >= 4){
-        tipoDePago = prompt(`Ingresa una opcion valida
-        1. Efectivo.
-        2. Debito.
-        3. Credito.
-        `)
-    }   
+// function validarEntrega (){
 
-    if (tipoDePago == 1){
-        alert(`Pago con Efectivo Ingresado`);
-        console.log(`Tipo de pago : Efectivo`);
-    }
-    else if (tipoDePago == 2){
-        alert(`Pago con Debito Ingresado`);
-        console.log(`Tipo de pago : Debito`);
-    }
-    else if (tipoDePago == 3){
-        alert(`Pago con Credito Ingresado`);
-        console.log(`Tipo de pago : Credito`);
-    }
+//     while(entrega != 1 && entrega != 2){
+//         entrega = prompt(`Ingresa una opcion valida
+//         1. Envio a domicilio : ${envio}
+//         2. Retiro en tienda : ${retiroEnTienda}
+//         `);
+//     }    
 
-    alert("Magicamente su pago se ha registrado correctamente");
-}
+//     if (entrega == 1){
+//         alert(`Envio a domicilio ${envio}`);
+//         console.log(`Tipo de entrega : Envio a domicilio`);
+//     }
+//     else if(entrega == 2){
+//         alert(`Retiro en tienda ${retiroEnTienda}`);
+//         console.log(`Tipo de entrega : Retiro en tienda`);
+//     }
 
-validarFormaDePago();
+//     return entrega;
+// }
 
-/*
-***************** NOTA*************************
-todos los resultados de la simulacion tambien
-se pueden ir observando en consola de esta
-manera no se pierde la informacion seleccionada
-despues de avanzar a la siguiente linea a menos
-que refresques la pagina.
-*/
+// validarEntrega();
+
+
+// // seleccionar tipo de pago
+
+// let tipoDePago = Number(prompt(`Elige el tipo de pago
+// 1. Efectivo.
+// 2. Debito.
+// 3. Credito.
+// `));
+
+// function validarFormaDePago (){
+
+//     while(tipoDePago <= 0  || tipoDePago >= 4){
+//         tipoDePago = prompt(`Ingresa una opcion valida
+//         1. Efectivo.
+//         2. Debito.
+//         3. Credito.
+//         `)
+//     }   
+
+//     if (tipoDePago == 1){
+//         alert(`Pago con Efectivo Ingresado`);
+//         console.log(`Tipo de pago : Efectivo`);
+//     }
+//     else if (tipoDePago == 2){
+//         alert(`Pago con Debito Ingresado`);
+//         console.log(`Tipo de pago : Debito`);
+//     }
+//     else if (tipoDePago == 3){
+//         alert(`Pago con Credito Ingresado`);
+//         console.log(`Tipo de pago : Credito`);
+//     }
+
+//     alert("Magicamente su pago se ha registrado correctamente");
+// }
+
+// validarFormaDePago();
+
+// /*
+// ***************** NOTA*************************
+// todos los resultados de la simulacion tambien
+// se pueden ir observando en consola de esta
+// manera no se pierde la informacion seleccionada
+// despues de avanzar a la siguiente linea a menos
+// que refresques la pagina.
+// */
